e52b8e263b4117b0adab2216ab6cdbd90676a63e
Ryan Warner <kentoe@gmail.com>
2023-12-15 20:20:21 +0000

    Fix issue with images at one point

--- wp-content/themes/manor/js/custom-functions.js
+++ wp-content/themes/manor/js/custom-functions.js
@@ -6,3 +6,10 @@ jQuery(window).load(function() {
         jQuery(('html, body')).scrollTop(jQuery(item1).offset().top - 30);
    }
 });
+
+// Ignore all images on site to pin, except for the ones with the class .sharethisimg
+// Allows control on what image to use for pin in the recipes page
+jQuery(document).ready(function(jQuery) {
+   jQuery("img").attr("data-pin-nopin", "true");  // blacklists all images
+   jQuery(".sharethisimg").removeAttr("data-pin-nopin", "true");  // whitelists all images with "sharethisimg" class
+});
